bd6403fa21b44487fe6354f4edca8cf37f6fbf65
nikko <xokkinx@gmail.com>
2022-09-27 14:46:48 -0700

    django

--- myproject/myworld/members/views.py
+++ myproject/myworld/members/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse("Hello world!")
